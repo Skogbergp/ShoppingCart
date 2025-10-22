501d009d785e33308686eebdf5d02ad5b81182e1
PatrikSkogberg <skogbergp@gmail.com>
2025-10-22 18:04:23 +0300

    Add multilingual support for shopping cart application and improve user prompts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Scanner;
@@ -6,18 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Locale locale;
-        ResourceBundle rb;
-        ShoppingCart shoppingCart = new ShoppingCart();
+        ResourceBundle rb = null;
+        int amountOfItems = 0;
+
+        double totalCost = 0.0;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Select language:");
         System.out.println("1. English");
         System.out.println("2. Finnish");
         System.out.println("3. Swedish");
-        System.out.println("5. Japanese");
+        System.out.println("4. Japanese");
 
         switch (sc.nextInt()) {
             case 1:
-              locale =  new Locale("en", "US") ;
+                locale =  new Locale("en", "US") ;
                 rb = setBundle(locale);
                 break;
             case 2:
@@ -32,7 +34,34 @@ public class Main {
                 locale =  new Locale("ja", "JP") ;
                 rb = setBundle(locale);
                 break;
+
+            default:
+                System.out.println("Invalid input");
         }
+
+
+        System.out.println(rb.getString("amountOfItems"));
+        amountOfItems = sc.nextInt();
+
+        double price = 0;
+        int quantity = 0;
+
+        for(int i = 0; i < amountOfItems; i++) {
+            System.out.println(rb.getString("Price"));
+            price = sc.nextDouble();
+
+
+            System.out.println(rb.getString("quantity"));
+            quantity = sc.nextInt();
+            totalCost += price * quantity;
+        }
+
+        System.out.println(rb.getString("totalCost") + totalCost);
+
+
+
+
+
 
 
 
